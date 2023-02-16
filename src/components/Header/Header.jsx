542c926152ba4968bf360c821e0514f40e5fa880
timohuennebeck <hello@timohuennebeck.com>
2023-02-16 16:55:05 +0100

    add option to find request line

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,18 +6,20 @@ import React from "react";
 // icons and images
 import logoImg from "../../assets/icons/spellwise-logo.svg";
 import profileImg from "../../assets/images/portrait.jpg";
+import googleImg from "../../assets/images/google-chrome-logo.png";
+import upgradeImg from "../../assets/icons/upgrade.svg";
 
 export default function Header() {
     return (
         <div className="header">
             <img className="header__logo" src={logoImg} alt="" />
             <nav className="header__nav">
-                <div className="header__nav-chrome">
-                    <img src="" alt="" />
+                {/* <div className="header__nav-chrome">
+                    <img className="header__nav-chrome-logo" src={googleImg} alt="" />
                     <p className="header__nav-chrome-cta">Download Extension</p>
-                </div>
+                </div> */}
                 <div className="header__nav-upgrade">
-                    <img src="" alt="" />
+                    <img className="header__nav-upgrade-logo" src={upgradeImg} alt="" />
                     <p className="header__nav-upgrade-cta">Upgrade</p>
                 </div>
                 <img className="header__nav-profile" src={profileImg} alt="" />
--- src/pages/EditorPage/EditorPage.jsx
+++ src/pages/EditorPage/EditorPage.jsx
@@ -1,13 +1,41 @@
 import "./EditorPage.scss";
 
 // libraries
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // components
 import TextEditorButtons from "../../components/TextEditorButtons/TextEditorButtons";
 
 export default function EditorPage() {
     const [userInput, setUserInput] = useState("");
+    const [currentWords, setCurrentWords] = useState(0);
+    const [currentCharacters, setCurrentCharacters] = useState(0);
+
+    useEffect(() => {
+        setCurrentWords(userInput.split(" ").length - 1);
+        setCurrentCharacters(userInput.split("").length);
+    }, [userInput]);
+
+    const handleRequest = (event) => {
+        if (event.key === "Enter" && userInput.includes("//")) {
+            // split the userInput string into an arr of lines
+            const lines = userInput.split("\n");
+
+            // find the index of the line that includes "//"
+            const lineIndex = lines.findIndex((line) => line.includes("//"));
+
+            if (lineIndex !== -1) {
+                // if a matching line is found
+                lines.splice(lineIndex, 1); // removes the line from the arr
+
+                // joins the remaining lines back into a single string
+                const updatedUserInput = lines.join("\n");
+
+                // update the state with the new userInput string
+                setUserInput(updatedUserInput);
+            }
+        }
+    };
 
     return (
         <div className="editor-page">
@@ -23,7 +51,9 @@ export default function EditorPage() {
                         <TextEditorButtons name="Expand" />
                     </div>
                 </div>
-                <div className="editor-page__nav-placeholder"></div>
+                <div className="editor-page__nav-placeholder">
+                    {/* placeholder to create the same width as the editor and suggestions sections  */}
+                </div>
             </nav>
             <div className="editor-page__box">
                 <div className="editor-page__box-editor">
@@ -32,21 +62,26 @@ export default function EditorPage() {
                         placeholder="Write or paste text here..."
                         value={userInput}
                         onChange={(e) => setUserInput(e.target.value)}
+                        onKeyDown={handleRequest}
                     />
                     <div className="editor-page__box-editor-choose">
-                        <div className="editor-page__box-editor-choose-headers">
-                            <p>H1</p>
-                            <p>H1</p>
-                            <p>H1</p>
-                            <p>H1</p>
-                        </div>
-                        <div className="editor-page__box-editor-choose-words">
-                            <p>Words</p>
+                        <div className="editor-page__box-editor-choose-box">
+                            <div className="editor-page__box-editor-choose-box-headers">
+                                <p>H1</p>
+                                <p>H1</p>
+                                <p>H1</p>
+                                <p>H1</p>
+                            </div>
+                            <div className="editor-page__box-editor-choose-box-words">
+                                <p>
+                                    {currentWords} Words ({currentCharacters} Characters)
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="editor-page__box-suggestions">
-                    <div className="editor-page__box-suggestions-data">123</div>
+                    <span className="editor-page__box-suggestions-data">Your Suggestions</span>
                 </div>
             </div>
         </div>
